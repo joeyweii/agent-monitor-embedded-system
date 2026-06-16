997820a0b2df791bfd4854e97b01c8020f621880
joeyweii <joey.cywei@gmail.com>
2026-06-16 18:27:32 +0800

    fix main loop event flag race conditions

--- src/main.cpp
+++ src/main.cpp
@@ -21,22 +21,22 @@ int main() {
 
         // Process UART Communication
         if (protocol_event_flag) {
-            handle_protocol_event();
             protocol_event_flag = false;
+            handle_protocol_event();
             ui_dirty_flag = true;
         }
 
         // Process Button Event
         if (button_event_flag) {
-            handle_button_event(target_button_gpio);
             button_event_flag = false;
+            handle_button_event(target_button_gpio);
             ui_dirty_flag = true;
         }
 
         // Render
         if (ui_dirty_flag && !display_is_busy()) {
-            ui_update();
             ui_dirty_flag = false;
+            ui_update();
         }
 
         // Wait for interrupt (DMA, Serial, or Timer)
